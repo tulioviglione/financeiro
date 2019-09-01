9f08582d771f526e344a68fe6fd977490aee3e36
Tulio Viglione <tulio.l.viglione@gmail.com>
2019-09-01 10:11:35 -0300

    criado UsuarioDTO

---
+++ src/main/java/com/financeiro/api/dtos/UsuarioDTO.java
@@ -0,0 +1,95 @@
+package com.financeiro.api.dtos;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.financeiro.api.enteties.Usuario;
+import com.financeiro.api.enums.SituacaoUsuarioEnum;
+
+public class UsuarioDTO {
+
+	private Long id;
+	private String nome;
+	private String sobrenome;
+	private String login;
+	private String email;
+	private String senha;
+	private SituacaoUsuarioEnum situacao;
+
+	public UsuarioDTO() {
+		// construtor padrão
+	}
+	
+	public UsuarioDTO(Usuario usuario) {
+		super();
+		this.id = usuario.getId();
+		this.nome = usuario.getNome();
+		this.sobrenome = usuario.getSobrenome();
+		this.login = usuario.getLogin();
+		this.email = usuario.getEmail();
+		this.situacao = usuario.getSituacao();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	@NotNull(message = "{login.nulo}")
+	@Size(min = 1, message = "{login.vazio}")
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	@NotNull(message = "{email.nulo}")
+	@Size(min = 1, message = "{email.vazio}")
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@NotNull(message = "{senha.nulo}")
+	@Size(min = 8, max = 16, message = "{senha.invalida}")
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public SituacaoUsuarioEnum getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(SituacaoUsuarioEnum situacao) {
+		this.situacao = situacao;
+	}
+
+}
