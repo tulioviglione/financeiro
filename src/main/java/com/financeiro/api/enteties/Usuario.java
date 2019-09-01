6facb08c96ee904be9585a41cd47f2522c0d7d74
Tulio Viglione <tulio.l.viglione@gmail.com>
2019-09-01 10:06:23 -0300

    entidade Usuario criada

---
+++ src/main/java/com/financeiro/api/enteties/Usuario.java
@@ -0,0 +1,105 @@
+package com.financeiro.api.enteties;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.financeiro.api.dtos.UsuarioDTO;
+import com.financeiro.api.enums.SituacaoUsuarioEnum;
+
+/**
+ * 
+ * Entidade Usuario
+ *
+ * @author Tulio Viglione
+ */
+@Entity
+@Table(name = "usuario")
+public class Usuario extends Generics implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "NOME", length = 50)
+	private String nome;
+
+	@Column(name = "SOBRENOME", length = 50)
+	private String sobrenome;
+
+	@Column(name = "LOGIN", length = 100, nullable = false, unique = true)
+	private String login;
+
+	@Column(name = "EMAIL", length = 150, nullable = false, unique = true)
+	private String email;
+
+	@Column(name = "SENHA", length = 255, nullable = false)
+	private String senha;
+
+	@Column(name = "SITUACAO", length = 9, nullable = false)
+	private SituacaoUsuarioEnum situacao;
+
+	public Usuario() {
+		// construtor padrão
+	}
+	
+	public Usuario(UsuarioDTO dto) {
+		super();
+		setId(dto.getId());
+		this.nome = dto.getNome();
+		this.sobrenome = dto.getSobrenome();
+		this.login = dto.getLogin();
+		this.email = dto.getEmail();
+		this.senha = dto.getSenha();
+		this.situacao = dto.getSituacao();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public SituacaoUsuarioEnum getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(SituacaoUsuarioEnum situacao) {
+		this.situacao = situacao;
+	}
+
+}
