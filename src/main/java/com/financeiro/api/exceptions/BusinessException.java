fb7e0325a5f2384156b207d0d25619c6da9f76d3
Tulio L. Viglione <tulio.l.viglione@gmail.com>
2019-12-23 17:06:18 -0300

    Adicionado exceção personalizada

---
+++ src/main/java/com/financeiro/api/exceptions/BusinessException.java
@@ -0,0 +1,23 @@
+package com.financeiro.api.exceptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BusinessException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private static final Logger LOG = LoggerFactory.getLogger(BusinessException.class);
+	
+	public BusinessException(String message) {
+        super(message);
+    }
+
+	public BusinessException(String message, Throwable e) {
+		super(message, e);
+		LOG.error(e.getMessage());
+	}
+}
